da7f24ecdfc9e2a302084e0862e99d7c9bd92166
stanekTM <44894972+stanekTM@users.noreply.github.com>
2021-03-23 11:22:49 +0100

    Initial commit

---
+++ Bayang.ino
@@ -0,0 +1,267 @@
+/*
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License.
+ If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define BAYANG_BIND_COUNT       1000
+#define BAYANG_PACKET_PERIOD    2000
+#define BAYANG_PACKET_SIZE      15
+#define BAYANG_RF_NUM_CHANNELS  4
+#define BAYANG_RF_BIND_CHANNEL  0
+#define BAYANG_ADDRESS_LENGTH   5
+
+static uint8_t Bayang_rf_chan = 0;
+static uint8_t Bayang_rf_channels[BAYANG_RF_NUM_CHANNELS] = {0};
+static uint8_t Bayang_rx_tx_addr[BAYANG_ADDRESS_LENGTH];
+
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+uint32_t process_Bayang()
+{
+  uint32_t timeout = micros() + BAYANG_PACKET_PERIOD;
+  
+  Bayang_receive_packet();
+  
+  return timeout;
+}
+
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+void Bayang_init()
+{
+  const uint8_t bind_address[] = {0, 0, 0, 0, 0};
+//  const uint8_t bind_address[BAYANG_ADDRESS_LENGTH] = {0, 0, 0, 0, 0}; //goebish
+  
+  uint8_t i;
+  for(i=0; i < BAYANG_ADDRESS_LENGTH; i++)
+  {
+    Bayang_rx_tx_addr[i] = random() & 0xff;
+  }
+  
+  Bayang_rf_channels[0] = 0x00;
+  
+  for(i = 1; i < BAYANG_RF_NUM_CHANNELS; i++)
+  {
+    Bayang_rf_channels[i] = random() % 0x42;
+  }
+
+/*
+    //pevná adresa goebish
+    const uint8_t bind_address[] = {0, 0, 0, 0, 0};
+    memcpy(Bayang_rx_tx_addr, transmitterID, 4);
+    Bayang_rx_tx_addr[4] = Bayang_rx_tx_addr[0] ^ 0xff;
+    Bayang_rf_channels[0] = 0x00;
+    uint8_t i;
+    for(i=1; i<BAYANG_RF_NUM_CHANNELS; i++)
+    {
+      Bayang_rf_channels[i] = transmitterID[i] % 0x42;
+    }
+*/
+
+  NRF24L01_Initialize();
+  NRF24L01_SetTxRxMode(TX_EN);
+  NRF24L01_SetTxRxMode(RX_EN);
+  XN297_SetTXAddr(bind_address, BAYANG_ADDRESS_LENGTH);
+  XN297_SetRXAddr(bind_address, BAYANG_ADDRESS_LENGTH);
+  NRF24L01_FlushTx();
+  NRF24L01_FlushRx();
+  NRF24L01_WriteReg(NRF24L01_01_EN_AA, 0x00);      // No Auto Acknowldgement on all data pipes
+  NRF24L01_WriteReg(NRF24L01_02_EN_RXADDR, 0x01);
+  NRF24L01_WriteReg(NRF24L01_03_SETUP_AW, 0x03);
+  NRF24L01_WriteReg(NRF24L01_11_RX_PW_P0, BAYANG_PACKET_SIZE); // rx pipe 0
+  NRF24L01_WriteReg(NRF24L01_04_SETUP_RETR, 0x00); // no retransmits
+  NRF24L01_SetBitrate(NRF24L01_BR_1M);             // 1Mbps
+  NRF24L01_SetPower(RF_POWER);
+  NRF24L01_Activate(0x73);                         // Activate feature register
+  NRF24L01_WriteReg(NRF24L01_1C_DYNPD, 0x00);      // Disable dynamic payload length on all pipes
+  NRF24L01_WriteReg(NRF24L01_1D_FEATURE, 0x01);
+  NRF24L01_Activate(0x73);
+  
+  XN297_Configure(_BV(NRF24L01_00_EN_CRC) | _BV(NRF24L01_00_CRCO) | _BV(NRF24L01_00_PWR_UP) | _BV(NRF24L01_00_PRIM_RX));
+  NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+  NRF24L01_FlushRx();
+  
+  delay(150);
+}
+
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+void Bayang_bind()
+{
+  int bind_count = 0;
+  uint8_t bind_packet[BAYANG_PACKET_SIZE] = {0};
+  uint32_t timeout;
+  
+  digitalWrite(pin_LED, LOW);
+  
+  NRF24L01_WriteReg(NRF24L01_05_RF_CH, BAYANG_RF_BIND_CHANNEL);
+  
+  while(bind_count < 10)
+  {
+  timeout = millis() + 5;
+    while(millis() < timeout)
+    {
+      delay(1);
+      
+      // data received from tx
+      if(NRF24L01_ReadReg(NRF24L01_07_STATUS) & _BV(NRF24L01_07_RX_DR))
+      {
+        XN297_ReadPayload(packet, BAYANG_PACKET_SIZE);
+        NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70); // Clear data ready, data sent, and retransmit
+        NRF24L01_FlushRx();
+        
+        if(packet[0] == 0xA4)
+        {
+          if (0 == bind_count)
+          {
+            memcpy(bind_packet, packet, BAYANG_PACKET_SIZE);
+            ++bind_count;
+          }
+          else
+          {
+            if (0 == memcmp(bind_packet, packet, BAYANG_PACKET_SIZE))
+            ++bind_count;
+          }
+        }
+        break;
+      }
+    }
+  }
+  
+  memcpy(Bayang_rx_tx_addr, &  packet[1], 5);
+  memcpy(Bayang_rf_channels, & packet[6], 4);
+  transmitterID[0] = packet[10];
+  transmitterID[1] = packet[11];
+
+  XN297_SetTXAddr(Bayang_rx_tx_addr, BAYANG_ADDRESS_LENGTH);
+  XN297_SetRXAddr(Bayang_rx_tx_addr, BAYANG_ADDRESS_LENGTH);
+
+  NRF24L01_WriteReg(NRF24L01_05_RF_CH, Bayang_rf_channels[Bayang_rf_chan++]);
+  NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+  NRF24L01_FlushRx();
+  
+  digitalWrite(pin_LED, HIGH);
+}
+
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+void Bayang_receive_packet()
+{
+  static bool is_bound = false;
+  static uint16_t failsafe_counter = 0;
+
+  // data received from tx
+  if(NRF24L01_ReadReg(NRF24L01_07_STATUS) & _BV(NRF24L01_07_RX_DR))
+  {
+    is_bound = true;
+    failsafe_counter = 0;
+    
+    int sum = 0;
+    
+    uint16_t roll, pitch, yaw, throttle, output1, output2, output3, output4;
+    
+    XN297_ReadPayload(packet, BAYANG_PACKET_SIZE);
+    
+    if (packet[0] == 0xA4) //0xA4, 0xA3 telemetry
+    {
+      // bind packet
+      Serial.println("bind packet");
+    }
+    else if (packet[0] == 0xA5) //0xA5
+    {
+      // data packet
+      for(int i = 0; i < 14; i++)
+      {
+        sum += packet[i];
+      }
+      
+      if ((sum & 0xFF) == packet[14])
+      {
+        // checksum OK
+        roll     = (packet[4]  & 0x0003) * 256 + packet[5];
+        pitch    = (packet[6]  & 0x0003) * 256 + packet[7];
+        yaw      = (packet[10] & 0x0003) * 256 + packet[11];
+        throttle = (packet[8]  & 0x0003) * 256 + packet[9];
+        output1  = (packet[2]  & 0x08);
+        output2  = (packet[2]  & 0x01);
+        output3  = (packet[2]  & 0x02);
+        output4  = (packet[3]  & 0x80);
+        
+//        Serial.println(roll,     DEC);
+//        Serial.println(pitch,    DEC);
+//        Serial.println(yaw,      DEC);
+//        Serial.println(throttle, DEC);
+//        Serial.println(output1,  DEC);
+//        Serial.println(output2,  DEC);
+//        Serial.println(output3,  DEC);
+//        Serial.println(output4,  DEC);
+
+        int value_servo1 = 0, value_servo2 = 0, value_servo3 = 0, value_servo4 = 0;
+        value_servo1 = map(roll,     0, 1023, 1000, 2000);
+        value_servo2 = map(pitch,    0, 1023, 1000, 2000);
+        value_servo3 = map(yaw,      0, 1023, 1000, 2000);
+        value_servo4 = map(throttle, 0, 1023, 1000, 2000);
+        
+        servo1.writeMicroseconds(value_servo1);
+        servo2.writeMicroseconds(value_servo2);
+        servo3.writeMicroseconds(value_servo3);
+        servo4.writeMicroseconds(value_servo4);
+        
+        digitalWrite(pin_button_flip,     output1);
+        digitalWrite(pin_button_return,   output2);
+        digitalWrite(pin_button_headless, output3);
+        digitalWrite(pin_button_invert,   output4);
+      }
+      else
+      {
+        //checksum FAIL
+        Serial.println("checksum FAIL");
+      }
+      NRF24L01_WriteReg(NRF24L01_05_RF_CH, Bayang_rf_channels[Bayang_rf_chan++]);
+      Bayang_rf_chan %= sizeof(Bayang_rf_channels);
+      NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+      NRF24L01_FlushRx();
+    }
+    else
+    {
+      Serial.print("Unrecognized packet: ");
+      Serial.println(packet[0], HEX);
+    }
+  }
+  else if (is_bound)
+  {
+    ++failsafe_counter;
+    
+    if (failsafe_counter > 1000)
+    {
+      is_bound = false;
+      failsafe_counter = 0;
+      
+      // neutral servo position
+      servo1.write(90);
+      servo2.write(90);
+      servo3.write(90);
+      servo4.write(90);
+      
+      // enable rebinding:
+      extern bool reset;
+      reset = true;
+      Serial.println("reset");
+    }
+  }
+}
+ 
---
+++ RX_Bayang_Servo.ino
@@ -0,0 +1,116 @@
+/*
+ * Thanks to the original developers "goebish" and "bikemike".
+ * https://github.com/bikemike/nrf24_multipro/blob/rx_mode/nRF24_multipro/Bayang.ino
+*/
+
+#include <util/atomic.h>
+#include <EEPROM.h>
+#include "iface_nrf24l01.h"
+#include <Servo.h>
+
+Servo servo1, servo2, servo3, servo4;
+
+//free pins
+//pin                       0
+//pin                       1
+//pin                       7
+//pin                       8
+//pin                       9
+//pin                       A5
+//pin                       A6
+//pin                       A7
+
+#define pin_button_flip     10
+#define pin_button_return   11
+#define pin_button_headless 12
+#define pin_button_invert   13
+
+//pins for servos
+#define pin_servo1          3
+#define pin_servo2          4
+#define pin_servo3          5
+#define pin_servo4          6
+
+//LED RX RF on/off
+#define pin_LED             2
+
+//SPI Comm.pins with nRF24L01
+//#define pin_CE              A0 //comment if you connect the pin CE to 3.3V
+#define pin_CSN             A1
+#define pin_SCK             A2
+#define pin_MOSI            A3
+#define pin_MISO            A4
+
+// SPI outputs
+#define CE_on    //PORTC |= _BV(0)  //PC0, comment if you connect the pin CE to 3.3V
+#define CE_off   //PORTC &= ~_BV(0) //PC0, comment if you connect the pin CE to 3.3V
+#define CS_on    PORTC |= _BV(1)  //PC1
+#define CS_off   PORTC &= ~_BV(1) //PC1
+#define SCK_on   PORTC |= _BV(2)  //PC2
+#define SCK_off  PORTC &= ~_BV(2) //PC2
+#define MOSI_on  PORTC |= _BV(3)  //PC3
+#define MOSI_off PORTC &= ~_BV(3) //PC3
+// SPI input
+#define  MISO_on (PINC & _BV(4))  //PC4
+
+//#define RF_POWER TX_POWER_5mW
+#define RF_POWER TX_POWER_20mW
+//#define RF_POWER TX_POWER_80mW 
+//#define RF_POWER TX_POWER_158mW
+
+uint8_t transmitterID[4];
+uint8_t packet[32];
+static bool reset = true;
+
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+void setup()
+{
+//  Serial.begin(115200); //print value ​​on a serial monitor
+  Serial.println("Start");
+
+  pinMode(pin_LED, OUTPUT);
+  digitalWrite(pin_LED, LOW); //start LED off
+  
+//  pinMode(pin_CE, OUTPUT); //comment if you connect the pin CE to 3.3V
+  pinMode(pin_CSN, OUTPUT);
+  pinMode(pin_SCK, OUTPUT);
+  pinMode(pin_MOSI, OUTPUT);
+  pinMode(pin_MISO, INPUT);
+  
+  pinMode(pin_button_flip, OUTPUT);
+  pinMode(pin_button_return, OUTPUT);
+  pinMode(pin_button_headless, OUTPUT);
+  pinMode(pin_button_invert, OUTPUT);  
+  
+  servo1.attach(pin_servo1);
+	servo2.attach(pin_servo2);
+  servo3.attach(pin_servo3);
+  servo4.attach(pin_servo4);
+}
+
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+//************************************************************************************************************************************************************************
+void loop()
+{
+  uint32_t timeout = 0;
+  
+  //reset/rebind
+  if(reset)
+  {
+    reset = false;
+    
+    NRF24L01_Reset();
+    NRF24L01_Initialize();
+    Bayang_init();
+    Bayang_bind();
+  }
+  
+  timeout = process_Bayang();
+  
+  // wait before sending next packet
+  while(micros() < timeout);
+}
+  
