c425d26d3b2b3ca82a5bf675321ea1045bc298b3
stanekTM <44894972+stanekTM@users.noreply.github.com>
2025-04-14 11:11:51 +0200

    Update RX_Bayang_Servo.ino

--- RX_Bayang_Servo.ino
+++ RX_Bayang_Servo.ino
@@ -1,7 +1,16 @@
-/*
- * Thanks to the original developers "goebish" and "bikemike".
- * https://github.com/bikemike/nrf24_multipro/blob/rx_mode/nRF24_multipro/Bayang.ino
-*/
+
+//************************************************************************************************************************
+// RC receiver with "Bayang" protocol
+//************************************
+//
+// ... works with OpenAVRc https://github.com/Ingwie/OpenAVRc_Dev
+// and Multiprotocol https://github.com/pascallanger/DIY-Multiprotocol-TX-Module
+//
+// Includes nRF24L01+ transceiver and ATmega328P processor, 4x servo output and 4x logic output.
+//
+// Thanks to the original developers "goebish" and "bikemike" https://github.com/bikemike/nrf24_multipro/tree/rx_mode
+//************************************************************************************************************************
+
 
 #include <util/atomic.h>
 #include "iface_nrf24l01.h"
